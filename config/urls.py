37610276ff3df798d896ea1676018068a2b9e20d
Sezim <sezimzhaparova06@gmail.com>
2025-08-01 17:37:05 +0600

    website

--- config/urls.py
+++ config/urls.py
@@ -26,3 +26,7 @@ urlpatterns = [
     path('',include('product.urls')),
     path('',include('user.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
